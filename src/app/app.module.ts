50db272b5a6f9c02ef1d7d33668300a3b1250b9c
jantunez <jantunez@sms-sudamerica.com>
2021-10-01 16:37:52 -0300

    Proyecto Angular.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 import { AppComponent } from './app.component';
+import { LocalidadesDetalleFormComponent } from './localidades-detalle/localidades-detalle-form/localidades-detalle-form.component';
+import { LocalidadesDetalleComponent } from './localidades-detalle/localidades-detalle.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LocalidadesDetalleComponent,
+    LocalidadesDetalleFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
